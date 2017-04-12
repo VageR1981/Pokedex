85274f241cc9c248ff83e2cce59e3fdd90628385
Vattai Gergely <vattai.gergely@fornax.hu>
2017-04-12 13:48:33 +0200

    add type query

--- html/script.js
+++ html/script.js
@@ -1,3 +1,24 @@
+var typeColor = {
+    "Normal": "A8A77A",
+    "Fire": "EE8130",
+    "Water": "6390F0",
+    "Electric": "F7D02C",
+    "Grass": "7AC74C",
+    "Ice": "96D9D6",
+    "Fighting": "C22E28",
+    "Poison": "A33EA1",
+    "Ground": "E2BF65",
+    "Flying": "A98FF3",
+    "Psychic": "F95587",
+    "Bug": "A6B91A",
+    "Rock": "B6A136",
+    "Ghost": "735797",
+    "Dragon": "6F35FC",
+    "Dark": "705746",
+    "Steel": "B7B7CE",
+    "Fairy": "D685AD"
+};
+
 function start() {
     console.log('start');
     var container = document.querySelector(".pokelist-container");
@@ -42,7 +63,9 @@ function startFetch() {
 
                 pokeDiv.setAttribute("class", "pokeflex-item");
                 pokeDiv.setAttribute("id", pokeId);
-
+                var pokemonArrayWithId = [];
+                pokemonArrayWithId[pokeId] = json.results[i];
+                console.log(pokemonArrayWithId);
                 //pokeDiv.onclick = clickHandler;
                 //console.log(splitArray);
 
